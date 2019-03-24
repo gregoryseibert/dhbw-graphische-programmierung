7b204ce2be8d661987606d3aeb5f834173f15afd
Gregory Seibert <gregory.hendrik.seibert@gmail.com>
2019-03-24 14:11:34 +0100

    Improve matlab and simulink models for overview

--- TPM-Matlab/SampleCurveDataAnalyzation.m
+++ TPM-Matlab/SampleCurveDataAnalyzation.m
@@ -12,36 +12,31 @@ end
 function [] = plotCurveData(tv, vfl, vfr, vrl, vrr, sw, RFL, tq, q)
     figure
 
-    subplot(5,2,[1 2]);
-    plot(tv, vfl, tv, vfr, tv, vrl, tv, vrr);
-    title('v overlay');
+    subplot(5,2,1);
+    plot(tv, vfl);
+    title('v - front left');
+
+    subplot(5,2,2);
+    plot(tv, vfr);
+    title('v - front right');
 
     subplot(5,2,3);
-    plot(tv, vfl);load('curve.mat')
-    title('v front left');
+    plot(tv, vrl);
+    title('v - rear left');
 
     subplot(5,2,4);
-    plot(tv, vfr);
-    title('v front right');
-
-    subplot(5,2,5);
-    plot(tv, vrl);
-    title('v rear left');
-
-    subplot(5,2,6);
     plot(tv, vrr);
-    title('v rear right');
+    title('v - rear right');
 
-    subplot(5,2,[7 8]);
+    subplot(5,2,[5 6]);
     plot(tv, sw);
     title('steering wheel signal');
+    
+    subplot(5,2,[7 8]);
+    plot(tq, q);
+    title('lateral acceleration');
 
     subplot(5,2,[9 10]);
     plot(tv, RFL);
-    title('curve radiuses');
-    
-    figure
-    
-    plot(tq, q);
-    title('lateral acceleration');
+    title('curve radius');
 end
